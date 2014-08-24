c8e01b0981d7a6f4771bbfbc4eceb2003a7dbbdd
Vladimir Yudintsev <yv87@ya.ru>
2014-08-24 15:21:15 +0600

    model header json structure

--- drunken-shame/xapp/models.py
+++ drunken-shame/xapp/models.py
@@ -44,16 +44,16 @@ class ModelFuncMixin(object):
                 ':', table.__name__.lower(), "-list"])
             local_fields = []
             for field in table._meta.local_fields:
-                local_fields.append([
-                field.db_type \
-                    .__str__().replace('<bound method ','') \
-                    [:table._meta.local_fields[1]\
-                        .db_type.__str__()\
-                        .replace('<bound method ','').find('.')],
-                field.verbose_name,
-                field.column,])
-            sheets.append([reverse(url_name), table._meta.verbose_name,
-                local_fields])
+                local_fields.append({
+                    'id': field.column,
+                    'name': field.verbose_name,
+                    'type': field.__class__.__name__.split('Field')[0]  
+                })
+            sheets.append({
+                'sheet': table._meta.verbose_name,
+                'url': reverse(url_name),
+                'fields': local_fields
+            })
         return sheets
 
 
